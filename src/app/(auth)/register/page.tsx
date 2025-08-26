c5751c6375e3a091742fbb33d21ae5bff7a6ff22
Arifin-MRX <arifinmrx@gmail.com>
2025-08-26 21:24:31 +0700

    Link & Navigation

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,7 +1,87 @@
+import Link from "next/link";
+
 export default function RegisterPage() {
   return (
-    <div>
-      <h1>Register</h1>
-    </div>
+    <>
+      <div className="flex min-h-screen items-center justify-center bg-gray-100 dark:bg-gray-900 px-4">
+        {/* Card */}
+        <div className="w-full max-w-md rounded-2xl bg-white dark:bg-gray-800 shadow-lg p-8">
+          {/* Logo */}
+          <div className="flex flex-col items-center">
+            <img
+              alt="Your Company"
+              src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500"
+              className="h-12 w-12"
+            />
+            <h2 className="mt-6 text-center text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+              Sign up to our platform
+            </h2>
+          </div>
+
+          {/* Form */}
+          <form action="#" method="POST" className="mt-8 space-y-6">
+            <div>
+              <label htmlFor="name" className="block text-sm font-medium text-gray-700 dark:text-gray-200">
+                Your full name
+              </label>
+              <input
+                id="name"
+                name="name"
+                type="text"
+                required
+                className="mt-2 block w-full rounded-md border border-gray-300 dark:border-gray-600 bg-gray-50 dark:bg-gray-700 px-3 py-2 text-gray-900 dark:text-white placeholder-gray-400 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+              />
+            </div>
+            <div>
+              <label htmlFor="email" className="block text-sm font-medium text-gray-700 dark:text-gray-200">
+                Email address
+              </label>
+              <input
+                id="email"
+                name="email"
+                type="email"
+                required
+                autoComplete="email"
+                className="mt-2 block w-full rounded-md border border-gray-300 dark:border-gray-600 bg-gray-50 dark:bg-gray-700 px-3 py-2 text-gray-900 dark:text-white placeholder-gray-400 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+              />
+            </div>
+
+            <div>
+              <div className="flex items-center justify-between">
+                <label htmlFor="password" className="block text-sm font-medium text-gray-700 dark:text-gray-200">
+                  Password
+                </label>
+                <Link href="#" className="text-sm font-semibold text-indigo-600 hover:text-indigo-500 dark:text-indigo-400">
+                  Forgot password?
+                </Link>
+              </div>
+              <input
+                id="password"
+                name="password"
+                type="password"
+                required
+                autoComplete="current-password"
+                className="mt-2 block w-full rounded-md border border-gray-300 dark:border-gray-600 bg-gray-50 dark:bg-gray-700 px-3 py-2 text-gray-900 dark:text-white placeholder-gray-400 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+              />
+            </div>
+
+            <button
+              type="submit"
+              className="w-full rounded-md bg-indigo-600 px-4 py-2 text-sm font-semibold text-white hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+            >
+              Sign up account
+            </button>
+          </form>
+
+          {/* Footer */}
+          <p className="mt-6 text-center text-sm text-gray-600 dark:text-gray-400">
+            Have registered?{' '}
+            <Link href="/login" className="font-semibold text-indigo-600 hover:text-indigo-500 dark:text-indigo-400">
+              Sign in
+            </Link>
+          </p>
+        </div>
+      </div>
+    </>
   );
 }
--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -1,17 +1,29 @@
+"use client";
 import Link from "next/link";
+import { usePathname, useRouter } from "next/navigation";
 
 export default function Navbar() {
+  const pathname = usePathname();
+  const router = useRouter();
+
   return (
-    <nav className="flex bg-gray-800 py-2 px-5">
-      <h1 className="text-white">Navbar</h1>
+    <nav className="flex bg-gray-800 py-2 px-5 justify-between">
+     <div className="flex items-center">
+       <h1 className="text-white">Navbar</h1>
       <ul className="flex ml-5">
        <Link href="/">
-        <li className="mr-3 text-blue-300 cursor-pointer">Home</li></Link>
+        <li className={`mr-3  cursor-pointer ${pathname === "/" ? "text-blue-300" : "text-white"}`}>Home</li></Link>
         <Link href="/about">
-        <li className="mr-3 text-blue-300 cursor-pointer">About</li></Link>
+        <li className={`mr-3  cursor-pointer ${pathname === "/about" ? "text-blue-300" : "text-white"}`}>About</li></Link>
         <Link href="/about/profile">
-        <li className="mr-3 text-blue-300 cursor-pointer">profile</li></Link>
+        <li className={`mr-3  cursor-pointer ${pathname === "/about/profile" ? "text-blue-300" : "text-white"}`}>profile</li></Link>
       </ul>
+     </div>
+     <div>
+      <button
+      onClick={() => router.push("/login")}
+      className="bg-blue-500 text-white py-1 px-3 rounded-md text-sm cursor-pointer">Login</button>
+     </div>
     </nav>
   );
 }
