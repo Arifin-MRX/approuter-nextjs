fc2fa23c6f06c7bfde5c4c9e2a7b357d5ebcadbb
Arifin-MRX <arifinmrx@gmail.com>
2025-08-29 22:17:08 +0700

    off eslint biar bisa menggunakan any

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -20,6 +20,12 @@ const eslintConfig = [
       "next-env.d.ts",
     ],
   },
+  {
+    rules: {
+      // Baris ini yang akan menonaktifkan aturan `no-explicit-any`
+      "@typescript-eslint/no-explicit-any": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
